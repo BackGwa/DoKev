3cbd0c23915a765cccf521f002cbcd3da15a0670
강찬영 <backgwa@icloud.com>
2024-07-18 22:59:24 +0900

    빌드 스크립트 추가

---
+++ DoKevEngine/build.ps1
@@ -0,0 +1,7 @@
+kotlinc src -include-runtime -d bin/dokev.jar
+
+if ($LASTEXITCODE -eq 0) {
+    Write-Host "✓ 빌드에 성공하였습니다." -ForegroundColor Green
+} else {
+    Write-Host "✘ 빌드에 문제가 생겼습니다!" -ForegroundColor Red
+}
---
+++ DoKevEngine/build.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+kotlinc src -include-runtime -d bin/dokev.jar
+
+if [ $? -eq 0 ]; then
+    echo "\033[32m✓ 빌드에 성공하였습니다.\033[0m"
+else
+    echo "\033[31m✘ 빌드에 문제가 생겼습니다!\033[0m"
+fi

49a855da357f33f5e5886f04a2c9bd219fcbd22a
강찬영 <backgwa@icloud.com>
2024-07-19 00:56:49 +0900

    빌드 후 파일 자동 실행

--- DoKevEngine/build.sh
+++ DoKevEngine/build.sh
@@ -4,6 +4,7 @@ kotlinc src -include-runtime -d bin/dokev.jar
 
 if [ $? -eq 0 ]; then
     echo "\033[32m✓ 빌드에 성공하였습니다.\033[0m"
+    java -jar bin/dokev.jar
 else
     echo "\033[31m✘ 빌드에 문제가 생겼습니다!\033[0m"
 fi

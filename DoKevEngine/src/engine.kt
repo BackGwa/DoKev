7b69deea09d136e754675fc95c2ea322de7ab6ea
강찬영 <backgwa@icloud.com>
2025-01-12 21:41:16 +0900

    Update engine.kt

--- DoKevEngine/src/engine.kt
+++ DoKevEngine/src/engine.kt
@@ -1,12 +1,37 @@
 package backgwa.dokev
 
 import backgwa.dokev.Debug
+import backgwa.dokev.Shell
 
 /* Main Program */
-fun main() {
-    // debugger test
+fun main(args: Array<String>) {
     val debug = Debug()
 
-    debug.PreRegister(0, "DEBUGGER TEST")
-    debug.StandardError()
+    // 인자가 비어있는 경우 인터렉티브 쉘을 실행합니다.
+    if (args.isEmpty()) {
+        // TODO : Interactive Shell
+    } else {
+        debug.PreRegister(0, args.joinToString(separator = " "))
+        when (args[0]) {
+            "-c", "--compile" -> {
+
+            }
+
+            "-i", "--interpret" -> {
+
+            }
+
+            "v", "--version" -> {
+
+            }
+
+            "-h", "--help" -> {
+
+            }
+
+            else -> {
+                debug.StandardError()
+            }
+        }
+    }
 }
